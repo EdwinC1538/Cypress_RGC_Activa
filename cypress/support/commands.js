3e97f9eda230db20a322c8521026159e32123317
EdwinC1538 <ecasallas@rgc.com.co>
2026-01-30 11:20:59 -0500

    UPDATE Commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,16 +1,16 @@
-Cypress.Commands.add("login", (users) =>{
-    cy.visit('https://desa.activa-it.net/');
-    cy.get('.close-btn-informativo').click();
-    cy.contains('Ingreso Usuarios').click();
-    cy.get('#loginForm').within(()=>{
-        cy.get('[name="user"]').type(users.username);
-        cy.get('[name="pass"]').type(users.password);
-    });
-    cy.get('#btnLogin').click();
-    })
+// Cypress.Commands.add("login", (users) =>{
+//     cy.visit('https://desa.activa-it.net/');
+//     cy.get('.close-btn-informativo').click();
+//     cy.contains('Ingreso Usuarios').click();
+//     cy.get('#loginForm').within(()=>{
+//         cy.get('[name="user"]').type(users.username);
+//         cy.get('[name="pass"]').type(users.password);
+//     });
+//     cy.get('#btnLogin').click();
+//     })
 
 Cypress.Commands.add("visitRDL", () =>{
-    cy.visit('https://desa.activa-it.net:8019/');
+    cy.visit('https://preprod.activa-it.net/');
     cy.get('.close-btn-informativo').click();
     cy.get('a[href="/App_Customs/Externas/RadicacionLight/RadicacionLight.aspx"]').first().click({ force: true })
 })
